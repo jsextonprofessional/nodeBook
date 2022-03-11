18884e2a1c4203fd2fb5bf1eb2adad63ccbcdb50
jsprofessional <jshuman@protonmail.com>
2022-03-11 09:07:06 -0600

    Added recipe_connections directory. Install cities package. Created main.js using cities.

--- messages.js
+++ messages.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let messages = [
+exports.messages = [
 	"A change of environment can be a good thing.",
 	"You will make it.",
 	"Just run with the code."
---
+++ recipe_connection/main.js
@@ -0,0 +1,3 @@
+const cities = require("cities");
+var myCity = cities.zip_lookup("75226");
+console.log(myCity);
