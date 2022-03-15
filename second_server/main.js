1d7cd63f42cca0e546b953f43011ac1ec34e00b4
jsprofessional <jshuman@protonmail.com>
2022-03-15 12:32:46 -0500

    Coded along through lesson5

--- second_server/main.js
+++ second_server/main.js
@@ -1,16 +1,38 @@
 const port = 3000,
 	http = require("http"),
 	httpStatus = require("http-status-codes"),
-	app = http.createServer();
+	app = http
+		.createServer((req, res) => {
+			res.writeHead(httpStatus.OK, {
+				"Content-Type": "text/html"
+			});
+			let responseMessage = "<h1>WELCOME</h1>";
+			res.end(responseMessage);
+		})
+		.listen(port);
 
 app.on ("request", (req, res) => {
+	var body = [];
+	req.on("data", (bodyData) => {
+		body.push(bodyData);
+	});
+	req.on("end", () => {
+		body = Buffer.concat(body).toString();
+		console.log(`Request Body Contents: ${body}`);
+	});
+
+	console.log(`Method: ${getJSONString(req.method)}`);
+	console.log(`URL: ${getJSONString(req.url)}`);
+	console.log(`Headers: ${getJSONString(req.headers)}`);
+
 	res.writeHead(httpStatus.OK, {
 		"Content-Type": "text/html"
 	});
-	console.log(req.method);
-	console.log(req.url);
-	console.log(req.headers);
 
+	// console.log(req.method);
+	// console.log(req.url);
+	// console.log(req.headers);
+	
 	let responseMessage = "<h1>This will show on the screen.</h1>";
 	res.end(responseMessage);
 });
@@ -19,5 +41,5 @@ const getJSONString = obj => {
 	return JSON.stringify(obj, null, 2);
 };
 
-app.listen(port);
+// app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
