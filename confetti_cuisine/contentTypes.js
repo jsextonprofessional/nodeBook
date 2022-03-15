028260d9a3f643a79c9006d830ca7e0f85cf71c8
jsprofessional <jshuman@protonmail.com>
2022-03-15 18:24:03 -0500

    Coded along through lesson7

---
+++ confetti_cuisine/contentTypes.js
@@ -0,0 +1,17 @@
+module.exports = {
+	html: {
+		"Content-Type": "text/plain"
+	},
+	js: {
+		"Content-Type": "text/plain"
+	},
+	jpg: {
+		"Content-Type": "text/js"
+	},
+	png: {
+		"Content-Type": "image/png"
+	},
+	css: {
+		"Content-Type": "text/css"
+	}
+};
