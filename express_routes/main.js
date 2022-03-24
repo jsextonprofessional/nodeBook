454c22ceddbc25184f5cc598b76d1848be6c7735
jsprofessional <jshuman@protonmail.com>
2022-03-24 18:54:36 -0500

    Coded through listing9.5

--- express_routes/main.js
+++ express_routes/main.js
@@ -1,28 +1,36 @@
+const homeController = require("./controllers/homeController");
+
 const port = 3000,
 	express = require("express"),
 	app = express();
 
-app.get("/items/:vegetable", (req, res) => {
-	let veg = req.params.vegetable;
-	res.send(`This is the page for ${veg}`);
-});
+	app.use((req, res, next) => {
+		console.log(`request made to: ${req.url}`);
+		next();
+	});
 
-app.use((req, res, next) => {
-	console.log(`request made to: ${req.url}`);
-	next();
-});
-
-app.use(express.urlencoded({
-	extended: false
-})
-);
-
-app.use(express.json());
-
-app.post("/", (req, res) => {
-	console.log(req.body);
-	console.log(req.query);
-	res.send("POST successful.");
+	app.use(express.urlencoded({
+		extended: false
+	})
+	);
+	
+	
+	// app.get("/items/:vegetable", (req, res) => {
+		// 	res.send(req.params.vegetable);
+		// })
+		
+		app.get("/items/:vegetable", (req, res) => {
+			let veg = req.params.vegetable;
+			res.send(`This is the page for ${veg}`);
+			
+			
+		app.use(express.json());
+	
+		app.post("/", (req, res) => {
+			console.log(req.body);
+			console.log(req.query);
+			res.send("POST successful.");
+		});
 });
 
 app.listen(port, () => {
